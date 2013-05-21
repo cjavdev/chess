992e5dd60229feee06d0c51f914222b0146d5a2f
App Academy Student <student@appacademy.io>
2013-05-21 11:55:48 -0700

    Modified board class, defined Square class

--- board.rb
+++ board.rb
@@ -9,7 +9,7 @@ class Board
                           #row to match index, pull value from COL_HASH
 
   def initialize
-    #calls generate board
+    @board = generate_board
   end
 
 
@@ -17,9 +17,54 @@ class Board
 
   def generate_board
     board = []
-    8.times do
-      board << [nil] * 8
+    8.times do |row_index|
+      board << []
+      8.times do |column_index|
+        board[row_index] << Square.new([row_index, column_index])
+      end
     end
+    board.each do |row|
+      row.each do |square|
+        square.set_neighbors
+      end
+    end
+    board
   end
 end
 
+
+class Square
+  attr_accessor :neighbors, :occupied
+  attr_reader :north, :south, :east, :west, :nw, :sw, :ne, :se, :location
+  alias_method :occupied?, :occupied
+
+  def initialize(location)
+    @location = location
+    @east = nil
+    @west = nil
+    @north = nil
+    @south = nil
+    @ne = nil
+    @sw = nil
+    @se = nil
+    @nw = nil
+
+  end
+
+  def set_neighbors
+    row, column  = @location
+    @south = [(row+1), column] if is_in_board?([(row+1), column])
+    @north = [(row-1), column] if is_in_board?([(row-1), column])
+    @east = [row, column+1] if is_in_board?([row, column+1])
+    @west = [row, column-1] if is_in_board?([row, column-1])
+    @se = [(row+1), column+1] if is_in_board?([(row+1), column+1])
+    @nw = [(row-1), column-1] if is_in_board?([(row-1), column-1])
+    @ne = [(row-1), column+1] if is_in_board?([(row-1), column+1])
+    @sw = [(row+1), column-1] if is_in_board?([(row+1), column-1])
+  end
+
+  def is_in_board?(coord)
+    coord.all? {|c| c.between?(0, 7)}
+  end
+
+end
