99b6d20ae337bc06fbbd2a87daab163f92b33691
App Academy Student <student@appacademy.io>
2013-05-21 13:49:11 -0700

    changed neighbor square assignment to use a hash

--- board.rb
+++ board.rb
@@ -1,5 +1,5 @@
 class Board
-
+  attr_reader :board
   COL_HASH = {a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7}
 
 
@@ -34,36 +34,40 @@ end
 
 
 class Square
-  attr_accessor :neighbors, :occupied
-  attr_reader :north, :south, :east, :west, :nw, :sw, :ne, :se, :location
+  DELTAS = {
+    :south => [1, 0],
+    :north => [-1, 0],
+    :east => [0, 1],
+    :west => [0, -1],
+    :se => [1, 1],
+    :nw => [-1, -1],
+    :ne => [-1, 1],
+    :sw => [1, -1]
+  }
+
+  attr_accessor :occupied
+  attr_reader :location, :neighbors
   alias_method :occupied?, :occupied
 
   def initialize(location)
     @location = location
-    @east = nil
-    @west = nil
-    @north = nil
-    @south = nil
-    @ne = nil
-    @sw = nil
-    @se = nil
-    @nw = nil
-
+    @neighbors = Hash.new { nil }
   end
 
   def set_neighbors
-    row, column  = @location
-    @south = [(row+1), column] if is_in_board?([(row+1), column])
-    @north = [(row-1), column] if is_in_board?([(row-1), column])
-    @east = [row, column+1] if is_in_board?([row, column+1])
-    @west = [row, column-1] if is_in_board?([row, column-1])
-    @se = [(row+1), column+1] if is_in_board?([(row+1), column+1])
-    @nw = [(row-1), column-1] if is_in_board?([(row-1), column-1])
-    @ne = [(row-1), column+1] if is_in_board?([(row-1), column+1])
-    @sw = [(row+1), column-1] if is_in_board?([(row+1), column-1])
+    row, col  = @location
+
+    DELTAS.each do |key, value|
+      new_row = value[0] + row
+      new_col = value[1] + col
+
+      if in_board? ([new_row, new_col])
+        @neighbors[key] = [new_row, new_col]
+      end
+    end
   end
 
-  def is_in_board?(coord)
+  def in_board?(coord)
     coord.all? {|c| c.between?(0, 7)}
   end
 
