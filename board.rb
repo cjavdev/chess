4101c5e76b6cff91b45b4c4f1eafd1d126b03145
App Academy Student <student@appacademy.io>
2013-05-22 10:45:51 -0700

    Finished Pieces and refactored Board class

--- board.rb
+++ board.rb
@@ -1,16 +1,7 @@
 class Board
 
-  attr_reader :board
-  DELTAS = {
-    :south => [1, 0],
-    :north => [-1, 0],
-    :east => [0, 1],
-    :west => [0, -1],
-    :se => [1, 1],
-    :nw => [-1, -1],
-    :ne => [-1, 1],
-    :sw => [1, -1]
-  }
+  attr_reader :squares
+
 
   COL_HASH = {a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7}
 
@@ -21,17 +12,17 @@ class Board
                           #row to match index, pull value from COL_HASH
 
   def initialize
-    @board = generate_board
+    @squares = generate_squares
   end
 
-  def pos(position)
-    row, col = position
-
-    @board[row][col]
+  def [](row, col)
+    @squares[row][col]
   end
 
-  def self.deltas
-    DELTAS
+  def []=(row, col, piece)
+    old_row, old_col = piece.location
+    @squares[old_row][old_col] = nil
+    @squares[row][col] = piece
   end
 
   def self.in_board?(coord)
@@ -39,63 +30,57 @@ class Board
   end
 
   private
-
-    def generate_board
-      board = []
+    def generate_squares
+      squares = []
       8.times do |row_index|
-        board << []
+        squares << []
         8.times do |column_index|
-          board[row_index] << Square.new([row_index, column_index])
+          squares[row_index] << nil
         end
       end
-      board.each do |row|
-        row.each do |square|
-          square.set_neighbors
-        end
-      end
-      board
+      squares
     end
 
 
 end
 
 
-class Square
-
-  #get rid of this, unneccessary. Make sure to change references in pieces classes
-
-
-  attr_accessor :occupied,
-  attr_reader :location, :neighbors
-  attr_writer :occupied_by
-  alias_method :occupied?, :occupied
-
-  def initialize(location)
-    @location = location
-    @neighbors = Hash.new { nil }
-    @occupied_by = nil
-  end
-
-  def set_neighbors
-    row, col  = @location
-
-    DELTAS.each do |key, value|
-      new_row = value[0] + row
-      new_col = value[1] + col
-
-      if Square.in_board? ([new_row, new_col])
-        @neighbors[key] = [new_row, new_col]
-      end
-    end
-  end
-
-  def occupied_by
-    return self.occupied_by unless self.occupied_by.nil?
-    nil
-  end
-
-  def self.in_board?(coord)
-    coord.all? {|c| c.between?(0, 7)}
-  end
-
-end
+# class Square
+#
+#   #get rid of this, unneccessary. Make sure to change references in pieces classes
+#
+#
+#   attr_accessor :occupied,
+#   attr_reader :location, :neighbors
+#   attr_writer :occupied_by
+#   alias_method :occupied?, :occupied
+#
+#   def initialize(location)
+#     @location = location
+#     @neighbors = Hash.new { nil }
+#     @occupied_by = nil
+#   end
+#
+#   def set_neighbors
+#     row, col  = @location
+#
+#     DELTAS.each do |key, value|
+#       new_row = value[0] + row
+#       new_col = value[1] + col
+#
+#       if Square.in_board? ([new_row, new_col])
+#         @neighbors[key] = [new_row, new_col]
+#       end
+#     end
+#   end
+#
+#   def occupied_by
+#     return self.occupied_by unless self.occupied_by.nil?
+#     nil
+#   end
+#
+#   def self.in_board?(coord)
+#     coord.all? {|c| c.between?(0, 7)}
+#   end
+#
+# end
--- pieces.rb
+++ pieces.rb
@@ -1,6 +1,25 @@
 require 'set'
 require 'debugger'
 class Piece
+  DELTAS = {
+    :south => [1, 0],
+    :north => [-1, 0],
+    :east => [0, 1],
+    :west => [0, -1],
+    :se => [1, 1],
+    :nw => [-1, -1],
+    :ne => [-1, 1],
+    :sw => [1, -1],
+    :nne => [-2, 1],
+    :nnw => [-2, -1],
+    :een => [-1, 2],
+    :ees => [1, 2],
+    :sse => [2, 1],
+    :ssw => [2, -1],
+    :wwn => [-1, -2],
+    :wws => [1, -2]
+  }
+
   attr_accessor :location
   attr_reader :color, :board, :directions
 
@@ -8,10 +27,8 @@ class Piece
     @location = starting_location
     @color = color
     @board = board
-    @board.pos(@location).occupied_by = self #stand in for testing, occupied_by no longer exists
+    @board[*@location] = self #stand in for testing, occupied_by no longer exists
 
-    # @board[@location] = self
-    # Board#[]=
   end
 
 
@@ -19,18 +36,11 @@ class Piece
     possible_moves.include?(position)
   end
 
-end
-
-class Slider < Piece
-
-  # def valid_move?(destination, delta)
-  #   super(destination) && valid_path?(destination)
-  # end
-
-  def possible_moves #move to parent class, refactor to not be dependent on Square class
+  def possible_moves
     possible_moves = []
     @directions.each do |dir|
-      possible_moves += build_path(Board.deltas[dir])
+      #debugger
+      possible_moves += build_path(DELTAS[dir])
     end
     possible_moves
   end
@@ -40,16 +50,14 @@ class Slider < Piece
     row_offset, col_offset = dir
     row, col = [@location[0] + row_offset, @location[1] + col_offset]
     while Board.in_board?([row, col])
-      #debugger if [row, col] == [4,3]
-      square_contents = @board.pos([row, col]).occupied_by
-
+      square_contents = @board[row, col]
 
       if square_contents.nil? || square_contents.color != @color
         path << [row, col]
       end
 
       break unless square_contents.nil?
-      #BREAK IF SELF == STEPPER?
+      break if self.is_a?(Stepper)
       row += row_offset
       col += col_offset
     end
@@ -58,9 +66,12 @@ class Slider < Piece
 
 end
 
+class Slider < Piece
+
+end
+
 class Stepper < Piece
 
-  #write new build_path with new deltas for stepper pieces
 end
 
 
@@ -98,7 +109,7 @@ class King < Stepper
 
   def initialize(starting_location, color, board)
     super(starting_location, color, board)
-    @directions = Set.new [:north, :south, :east, :west, :se, :ne, :sw, :se]
+    @directions = Set.new [:north, :south, :east, :west, :se, :ne, :sw, :sw]
   end
 
 end
@@ -108,15 +119,17 @@ class Pawn < Stepper
     super(starting_location, color, board)
     case @color
     when :black
-      @directions = Set.new [:south, :se, :sw]
+      @directions = Set.new [:south]
     when :white
-      @directions = Set.new [:north, :ne, :nw]
+      @directions = Set.new [:north]
     end
   end
 end
 
 
-class Knight
-  #probably need to overwrite deltas
-
+class Knight < Stepper
+  def initialize(starting_location, color, board)
+    super(starting_location, color, board)
+    @directions = Set.new [:nne, :nnw, :een, :ees, :sse, :ssw, :wwn, :wws]
+  end
 end
